00fce49617d2e940b1b79e9ce3c1ca77c729c243
MarkDohan <122050255+MarkDohan@users.noreply.github.com>
2023-01-30 22:04:07 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('open')
 }
 
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
+function changeBg(){
+  var header = document.getElementById('scrolling')
+  var scrollValue = window.scrollY;
+  if(scrollValue < 15){
+    header.classList.remove('bgColor');
+  }else{
+    header.classList.add('bgColor');
+  }
 }
 
-
+window.addEventListener('scroll', changeBg)
